b6d9ae92738b2e61682d1a971187402f1039c4df
kucaleb <caleb.neil@ku.edu>
2024-11-05 17:56:43 -0600

    fixed paths

--- static/script.js
+++ static/script.js
@@ -1,10 +1,16 @@
-var sum = 100;
 var level1 = 100;
 var level2 = 100;
 var level3 = 100;
 var level4 = 100;
-var cont = document.getElementById('number');
-cont.innerHTML = num;
+var sum = level1 + level2 + level3 + level4;
+
+var sum_cont = document.getElementById('sum');
+var level1_cont = document.getElementById('level1_parking');
+var level2_cont = document.getElementById('level2_parking');
+var level3_cont = document.getElementById('level3_parking');
+var level4_cont = document.getElementById('level4_parking');
+
+sum_cont.innerHTML = sum;
 function main() {
     window.addEventListener('keydown', update_num)
     //window.addEventListener('sensor', update_num)
@@ -12,11 +18,42 @@ function main() {
 
 function update_num(event) {
     if (event.code == "ArrowUp") {
-        num++;
-        cont.innerHTML = num;
+        sum++;
     } else if (event.code == "ArrowDown") {
-        num--;
-        cont.innerHTML = num;
+        sum--;
     }
+    /*
+    if (event.code == "Level1_out") {
+        level1++;
+        level1_cont.innerHTML;
+    } else if (event.code == "Level1_in") {
+        level1--;
+        level1_cont.innerHTML;
+    } else if (event.code == "Level2_out") {
+        level2++;
+        level2_cont.innerHTML;
+    } else if (event.code == "Level2_in") {
+        level2--;
+        level2_cont.innerHTML;
+    } else if (event.code == "level3_out") {
+        level3++;
+        level3_cont.innerHTML;
+    } else if (event.code == "level3_in") {
+        level3--;
+        level3_cont.innerHTML;
+    } else if (event.code == "level4_out") {
+        level4++;
+        level4_cont.innerHTML;
+    } else if (event.code == "level4_in") {
+        level4--;
+        level4_cont.innerHTML;
+    }
+    level1_cont.innerHTML = level1;
+    level2_cont.innerHTML = level2;
+    level3_cont.innerHTML = level3;
+    level4_cont.innerHTML = level4;
+    */
+    sum_cont.innerHTML = sum;
+        
 }
 main()
