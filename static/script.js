cc908a7d0b6c7cd29175d8260fd297ab2b0e28df
kucaleb <caleb.neil@ku.edu>
2024-11-05 17:31:52 -0600

    added templates

--- static/script.js
+++ static/script.js
@@ -1,8 +1,13 @@
-var num = 100;
+var sum = 100;
+var level1 = 100;
+var level2 = 100;
+var level3 = 100;
+var level4 = 100;
 var cont = document.getElementById('number');
 cont.innerHTML = num;
 function main() {
     window.addEventListener('keydown', update_num)
+    //window.addEventListener('sensor', update_num)
 }
 
 function update_num(event) {
