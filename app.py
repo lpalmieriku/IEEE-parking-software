6efeaca32a2e2faa215f42dd12e843efb1b74528
lpalmieriku <l250p415@home.ku.edu>
2024-11-19 16:37:21 -0600

    Linked all of the webpages using flask templates

--- app.py
+++ app.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/level1/')
+def level1():
+    return render_template('level1.html')
+@app.route('/level2/')
+def level2():
+    return render_template('level2.html')
+@app.route('/level3/')
+def level3():
+    return render_template('level3.html')
+@app.route('/level4/')
+def level4():
+    return render_template('level4.html')
+@app.route('/games/')
+def games():
+    return render_template('game_dates.html')
+
 if __name__ == "main":
     app.run(debug=True)
